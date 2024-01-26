e0039383356fc67f5a42e4afc64f36a01722dcd1
Ties Stuij <ties@stuij.se>
2024-01-26 12:38:02 +0000

    change graph layout to add explicit entry and exit node
    also:
    - add some documentation
    - add `make-classified-graph` convenience fn

--- src/algos.lisp
+++ src/algos.lisp
@@ -1,13 +1,43 @@
 (in-package :opty)
 
+;; a literal graph is meant to be easily constructed from text,
+;; which can then be parsed into a more structured binary representation
+;;
+;; breakdown:
+;; - a list of
+;;   - a list of
+;;     - the node number
+;;     - a list of edges to other nodes represented as tuples
+;;       (successors in a directed graph)
+;;   - a tuple of the entry and exit node
+;;
+;; example:
+;;
+;; (defparameter maxcol-graph
+;;   '(((0 ((0 . 1) (0 . 5))) ;; B0
+;;      (1 ((1 . 2) (1 . 4))) ;; B1
+;;      (2 ((2 . 3) (2 . 6))) ;; B2
+;;      (3 ((3 . 2) (3 . 4))) ;; B3
+;;      (4 ((4 . 1) (4 . 5))) ;; B4
+;;      (5 ())                ;; B5
+;;      (6 ((6 . 3))))        ;; B6
+;;     (0 . 5)))              ;; entry and exit node respectively
+
 (defun graph-edges (graph)
-  (loop for e in graph
+  (loop for e in (car graph)
         append (car (cdr e))))
 
 (defun graph-nodes (graph)
-  (loop for e in graph
+  (loop for e in (car graph)
         collect (car e)))
 
+(defun graph-entry (graph)
+  (caadr graph))
+
+(defun graph-exit (graph)
+  (cdadr graph))
+
+;; digraph
 (defclass digraph-node ()
   ((id         :initarg :id         :accessor id)
    (successors :initarg :successors :accessor successors)))
@@ -18,10 +48,12 @@
       (format out "id:~A" id))))
 
 (defclass digraph ()
-  ((label  :initarg :label  :accessor label)
-   (nodes :initarg :nodes :accessor nodes)
-   (entry :initarg :entry :accessor entry)))
-
+  ((label    :initarg :label    :accessor label)
+   (nodes    :initarg :nodes    :accessor nodes)
+   (entry    :initarg :entry    :accessor entry)
+   (exit     :initarg :exit     :accessor exit)
+   (analyses :initarg :analyses :accessor analyses
+             :initform (make-hash-table))))
 
 (defun initialize-digraph-node (id)
   (make-instance 'digraph-node :id id
@@ -42,8 +74,20 @@
                              (list (gethash successor nodes))))))
     (make-instance 'digraph :label label
                             :nodes nodes
-                            :entry (gethash (car (car graph)) nodes))))
+                            :entry (gethash (graph-entry graph) nodes)
+                            :exit (gethash (graph-exit graph) nodes))))
 
+;; A classified digraph is a digraph that uses classify nodes, which add pre the
+;; and rpost slots to the nodes, which relate to their pre and reverse post
+;; order DFS number.
+;;
+;; These are populated in the classify-graph function. This function will also
+;; return a classification analysis, which will classify the nodes as tree edges,
+;; forward edges, cross edges or back edges.
+;;
+;; The `make-classified-graph` function will take a literal graph and name, will
+;; make a classification digraph, will do the classification and will store the
+;; classification analysis in the `analysis` hash table of the digraph.
 (defclass classify-node (digraph-node)
   ((pre   :initarg :pre   :accessor pre   :initform 0)
    (rpost :initarg :rpost :accessor rpost :initform 0)))
@@ -89,19 +133,26 @@
       (dfs (entry digraph))
       classification)))
 
+(defun make-classified-graph (name graph)
+  (let ((digraph (to-digraph name graph #'initialize-classify-node)))
+    (setf (gethash 'classification  (analyses digraph))
+          (classify-graph digraph))
+    digraph))
+
 ;; testing
 (defparameter maxcol-graph
-  '((0 ((0 . 1) (0 . 5))) ;; B0
-    (1 ((1 . 2) (1 . 4))) ;; B1
-    (2 ((2 . 3) (2 . 6))) ;; B2
-    (3 ((3 . 2) (3 . 4))) ;; B3
-    (4 ((4 . 1) (4 . 5))) ;; B4
-    (5 ())                ;; B5
-    (6 ((6 . 3)))))       ;; B6
+  '(((0 ((0 . 1) (0 . 5))) ;; B0
+     (1 ((1 . 2) (1 . 4))) ;; B1
+     (2 ((2 . 3) (2 . 6))) ;; B2
+     (3 ((3 . 2) (3 . 4))) ;; B3
+     (4 ((4 . 1) (4 . 5))) ;; B4
+     (5 ())                ;; B5
+     (6 ((6 . 3))))        ;; B6
+    (0 . 5)))              ;; entry and exit node respectively
 
 (define-test classify-graph
-  (let* ((digraph (to-digraph "maxcol" maxcol-graph #'initialize-classify-node))
-         (classification (classify-graph digraph)))
+  (let* ((digraph (make-classified-graph "maxcol" maxcol-graph))
+         (classification (gethash 'classification (analyses digraph))))
     (true (subsetp (tree-edges classification)
                    '((0 . 1) (1 . 2) (2 . 3) (3 . 4) (4 . 5) (2 . 6))
                    :test 'equal))
